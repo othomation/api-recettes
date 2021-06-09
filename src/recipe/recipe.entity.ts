26c117939539518ce0bbdcc0dc635b0f3f4d25cf
otho <contact@otho.bike>
2021-06-09 11:37:57 +0200

    (fix) spelling

--- src/recipe/recipe.entity.ts
+++ src/recipe/recipe.entity.ts
@@ -78,7 +78,7 @@ export class Recipe {
       "Les differents indicateurs de temps, dififculte etc.. d'une recette ! ",
   })
   @Column()
-  indicators: IIndicators;
+  indicateurs: IIndicators;
 
   @ApiProperty({
     example: ["four", "poele", "talent (c'est une blague le talent svp)"],
@@ -93,7 +93,7 @@ export class Recipe {
     description: "Oui ou non selon que cette recette est dans vos favoris.",
   })
   @Column()
-  favori: boolean;
+  favoris: boolean;
 
   @ApiProperty({
     example: 2,
