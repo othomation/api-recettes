24bb8653f7a285cf5f5b81c170060dbbf62971bc
otho <contact@otho.bike>
2021-06-09 16:49:26 +0200

    Update recipe.validator.ts

--- src/recipe/recipe.validator.ts
+++ src/recipe/recipe.validator.ts
@@ -2,14 +2,14 @@ import {
   registerDecorator,
   ValidationOptions,
   ValidationArguments,
-} from "class-validator";
+} from 'class-validator';
 
-import { Student } from "./student.entity";
+import { Student } from './student.entity';
 
 export function IsAValideStudentName(ValidationOptions?: ValidationOptions) {
   return function (object: Object, propertyname: string) {
     registerDecorator({
-      name: "IsAValideStudentName",
+      name: 'IsAValideStudentName',
       target: object.constructor,
       propertyName: propertyname,
       constraints: [],
@@ -19,7 +19,7 @@ export function IsAValideStudentName(ValidationOptions?: ValidationOptions) {
           return value in Student;
         },
         defaultMessage(message: ValidationArguments) {
-          let msg = [...Object.keys(Student)].join(" | ");
+          let msg = [...Object.keys(Student)].join(' | ');
           return (
             "Merci d'utiliser votre prenom, afin que chacun(e) puisse utiliser ses propres recettes de son côté s'iel le souhaite. --  " +
             msg
@@ -33,17 +33,17 @@ export function IsAValideStudentName(ValidationOptions?: ValidationOptions) {
 export function IsAValidDifficulte(ValidationOptions?: ValidationOptions) {
   return function (object: Object, propertyname: string) {
     registerDecorator({
-      name: "IsAValidDifficulte",
+      name: 'IsAValidDifficulte',
       target: object.constructor,
       propertyName: propertyname,
       constraints: [],
       options: ValidationOptions,
       validator: {
         validate(value: any, args: ValidationArguments) {
-          return value >= 0 && value < 6 && Number.isInteger(value);
+          return value >= 0 && value < 6;
         },
         defaultMessage(message: ValidationArguments) {
-          return "La difficulté doit être un nombre entier et se situer entre 0 (inclus) et 5 (inclus).";
+          return message + 'La difficulté doit être un nombre entier et se situer entre 0 (inclus) et 5 (inclus).';
         },
       },
     });
@@ -53,29 +53,18 @@ export function IsAValidDifficulte(ValidationOptions?: ValidationOptions) {
 export function IsAValidStep(ValidationOptions?: ValidationOptions) {
   return function (object: Object, propertyname: string) {
     registerDecorator({
-      name: "IsAValdStep",
+      name: 'IsAValdStep',
       target: object.constructor,
       propertyName: propertyname,
       constraints: [],
       options: ValidationOptions,
       validator: {
         validate(value: any, args: ValidationArguments) {
-          if (!Array.isArray(value)) return false;
-          let isValid = true;
-          value.forEach((val) => {
-            if (!isValid) return isValid;
-            isValid =
-              val &&
-              val.ordre &&
-              typeof val.ordre === "number" && val.ordre in [0,1,2,3,4,5] &&
-              val.description &&
-              typeof val.description === "string" &&
-              typeof val.obligatoire === "boolean";
-          });
-          return isValid;
+          // if (!Array.isArray(value)) return false;
+          return Array.isArray(value);
         },
         defaultMessage(message: ValidationArguments) {
-          return "A priori, il manquait soit le titre, soit la description, soit le numero, soit plusieurs ou toutes.";
+          return message + '[etapes] A priori, il manquait soit le titre, soit la description, soit le numero, soit plusieurs ou toutes.';
         },
       },
     });
@@ -85,28 +74,32 @@ export function IsAValidStep(ValidationOptions?: ValidationOptions) {
 export function IsAValidIngredient(ValidationOptions?: ValidationOptions) {
   return function (object: Object, propertyname: string) {
     registerDecorator({
-      name: "IsAValidIngredient",
+      name: 'IsAValidIngredient',
       target: object.constructor,
       propertyName: propertyname,
       constraints: [],
       options: ValidationOptions,
       validator: {
         validate(value: any, args: ValidationArguments) {
-          if (!Array.isArray(value)) return false;
-          let isValid = true;
-          value.forEach((val) => {
-            if (!isValid) return isValid;    isValid =
-              val &&
-              val.nom &&
-              typeof val.nom === "string" &&
-              val.quantite &&
-              typeof val.quantite === "number" &&
-              typeof val.obligatoire === "boolean" &&  (typeof val.unite === 'string' || typeof val.unite === 'number')/* val.unite in ['gr','kg','ml','cl','l'] */;
-           });
-          return isValid;
+          // if (!Array.isArray(value)) return false;
+          // let isValid = true;
+          // value.forEach((val) => {
+          //   if (!isValid) return isValid;
+          //   isValid =
+          //     val &&
+          //     val.nom &&
+          //     typeof val.nom === 'string' &&
+          //     val.quantite &&
+          //     typeof val.quantite === 'number' &&
+          //     typeof val.obligatoire === 'boolean' &&
+          //     (typeof val.unite === 'string' ||
+          //       typeof val.unite ===
+          //         'number') /* val.unite in ['gr','kg','ml','cl','l'] */;
+          // });
+          return Array.isArray(value);
         },
         defaultMessage(message: ValidationArguments) {
-          return "La propriété ingrédients doit être un tableau d'objets correspondant au schema.";
+          return message + "La propriété ingrédients doit être un tableau d'objets correspondant au schema.";
         },
       },
     });
